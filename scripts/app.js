138efafafe54a3f6e909aa0321ef6b576e17725e
Maria Mata <mariajo.mata@gmail.com>
2017-08-05 13:12:45 -0600

    added GET request to home page & finished styling

--- scripts/app.js
+++ scripts/app.js
@@ -1,27 +1,26 @@
 $(document).ready(function() {
-  $(".button-collapse").sideNav()
+  $('.button-collapse').sideNav()
   $('.chips').material_chip()
-  $(".button-collapse").sideNav()
+  $('.button-collapse').sideNav()
 
-  // const baseURL; // need heroku link from Yousif
+  const baseURL = 'https://young-peak-51032.herokuapp.com/skills'
 
-  // $.get(baseURL)
-  //   .then(appendSkills)
+  $.get(baseURL)
+    .then(appendSkills)
 
-  // $('.info').click(function() {
-  //   $('#slide-out').toggle()
-  // });
+  $('.info').click(function() {
+    $('#slide-out').toggle()
+  })
 });
 
 function appendSkills(data) {
   for (let i = 0; i < data.length; i++) {
     let skill = `<li>
                   <div class="chip">
-                    <i class="material-icons">code</i>
-                    ${data[i].name}
+                    <i class="material-icons">code</i> ${data[i].name}
                   </div>
                 </li>`
 
     $('#skillsList').append(skill)
   }
-}
+};
