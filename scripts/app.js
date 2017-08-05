5c5081d1d439f0c22ff348fea17457c940bf7c39
Maria Mata <mariajo.mata@gmail.com>
2017-08-05 12:15:39 -0600

    Style updates

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,27 @@
-$(document).ready(function(){
-  $(".button-collapse").sideNav();
-  $('.chips').material_chip();
+$(document).ready(function() {
+  $(".button-collapse").sideNav()
+  $('.chips').material_chip()
+  $(".button-collapse").sideNav()
 
+  // const baseURL; // need heroku link from Yousif
+
+  // $.get(baseURL)
+  //   .then(appendSkills)
+
+  // $('.info').click(function() {
+  //   $('#slide-out').toggle()
+  // });
 });
+
+function appendSkills(data) {
+  for (let i = 0; i < data.length; i++) {
+    let skill = `<li>
+                  <div class="chip">
+                    <i class="material-icons">code</i>
+                    ${data[i].name}
+                  </div>
+                </li>`
+
+    $('#skillsList').append(skill)
+  }
+}
