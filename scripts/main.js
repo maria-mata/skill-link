bce08bc0280a9f6fc02c09b328962f835d620f44
Maria Mata <mariajo.mata@gmail.com>
2017-08-06 16:01:38 -0600

    started on mentor match feature

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,18 @@
 const userId = 1
-const userURL = 'https://young-peak-51032.herokuapp.com/users/'
 
 $(document).ready(function() {
   $('.button-collapse').sideNav()
-  $('.modal').modal();
+  $('.modal').modal()
 
-  $.get(userURL + userId)
+  $.get(`https://young-peak-51032.herokuapp.com/users/${userId}`)
     .then(showUserProfile)
 
+  $.get(`https://young-peak-51032.herokuapp.com/users/matches/${userId}`)
+    .then(appendSkillMatches)
+
   $('#user-put').submit(userPutRequest)
+  // $('#skills-put').submit(skillsPutRequest)
+
 });
 
 function showUserProfile(data) {
@@ -18,24 +22,51 @@ function showUserProfile(data) {
   $('#email').val(data[0].email)
   $('#phone').val(data[0].phone)
   $('#bio').val(data[0].bio)
-}
+};
 
 function userPutRequest(event) {
   event.preventDefault()
-  // add photo later
-  let putURL = userURL + userId
   $.ajax({
-    url: putURL,
+    url: `https://young-peak-51032.herokuapp.com/users/${userId}`,
     type: 'PUT',
     data: {
+      // add photo later
       name: $('#name').val(),
       email: $('#email').val(),
       phone: $('#phone').val(),
       bio: $('#bio').val()
     },
     success: function(result) {
-        // Do something with the result
       showUserProfile(result)
     }
-  });
+  })
+};
+
+// function skillsPutRequest(event) {
+//
+// }
+
+function appendSkillMatches(data) {
+  for (let i = 0; i < data.length; i++) {
+    let skillMatch = `<li class="collection-item avatar">
+                  <img src="http://www.cdn.innesvienna.net//Content/user-default.png" alt="" class="circle">
+                  <span class="title">${data[i].name}</span>
+                  <p>Can Teach You: <br>
+                     Wants to Learn: ${data[i].skill_learn}
+                  </p>
+                  <a href="#!" class="secondary-content"><i class="material-icons">info_outline</i></a>
+                </li>`
+    $('#matches').append(skillMatch)
+  }
 }
+
+/* SUGGESTED MATCHES APPEND ELEMENT
+`            <li class="collection-item avatar">
+              <img src="http://www.cdn.innesvienna.net//Content/user-default.png" alt="" class="circle">
+              <span class="title">Title</span>
+              <p>First Line <br>
+                 Second Line
+              </p>
+              <a href="#!" class="secondary-content"><i class="material-icons">add</i></a>
+            </li>`
+*/
