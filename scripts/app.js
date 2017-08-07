fcb027b3892dd8acfb90a0dab1f80350c0333254
tkeesling <tyler.keesling@gmail.com>
2017-08-06 23:13:29 -0600

    added login/logout

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,53 @@
 $(document).ready(function() {
-  $('.chips').material_chip()
-  $('.button-collapse').sideNav()
+	$('.chips').material_chip()
+	$('.button-collapse').sideNav()
+	$('form.login').submit(logIn)
 
-  const baseURL = 'https://young-peak-51032.herokuapp.com/skills'
+	// const baseURL = 'https://young-peak-51032.herokuapp.com/'
+	const baseURL = 'http://localhost:8080/'
 
-  $.get(baseURL)
-    .then(appendSkills)
+	$.get(baseURL + 'skills')
+		.then(appendSkills)
 
-  $('.info').click(function() {
-    $('#slide-out').toggle()
-  })
+	$('.info').click(function() {
+		$('#slide-out').toggle()
+	})
 });
 
 function appendSkills(data) {
-  for (let i = 0; i < data.length; i++) {
-    let skill = `<li>
+	for (let i = 0; i < data.length; i++) {
+		let skill = `<li>
                   <div class="chip">
                     <i class="material-icons">code</i> ${data[i].name}
                   </div>
                 </li>`
 
-    $('#skillsList').append(skill)
-  }
+		$('#skillsList').append(skill)
+	}
+};
+
+function logIn(event) {
+	event.preventDefault()
+	const username = $('input[name=login-username]').val()
+	const password = $('input[name=login-password]').val()
+	const data = {
+		username,
+		password
+	}
+	$.post(`http://localhost:8080/auth/login`, data)
+		.then(res => {
+			if (res.error) {
+				alert(res.error)
+			} else {
+				// let token = parseJWT(res.data)
+				localStorage.setItem('token', res.data)
+				location.href = '/main.html'
+			}
+		})
+}
+
+function parseJWT(token) {
+	var base64Url = token.split('.')[1];
+	var base64 = base64Url.replace('-', '+').replace('_', '/');
+	return JSON.parse(window.atob(base64));
 };
--- scripts/main.js
+++ scripts/main.js
@@ -2,40 +2,47 @@ const userId = 1
 const userURL = 'https://young-peak-51032.herokuapp.com/users/'
 
 $(document).ready(function() {
-  $('.button-collapse').sideNav()
-  $('.modal').modal();
+	$('.button-collapse').sideNav()
+	$('.modal').modal();
+	$('.logOut').click(signOut)
 
-  $.get(userURL + userId)
-    .then(showUserProfile)
+	$.get(userURL + userId)
+		.then(showUserProfile)
 
-  $('#user-put').submit(userPutRequest)
+	$('#user-put').submit(userPutRequest)
 });
 
 function showUserProfile(data) {
-  // missing photo display
-  $('.card-title').text(data[0].name)
-  $('#name').val(data[0].name)
-  $('#email').val(data[0].email)
-  $('#phone').val(data[0].phone)
-  $('#bio').val(data[0].bio)
+	// missing photo display
+	$('.card-title').text(data[0].name)
+	$('#name').val(data[0].name)
+	$('#email').val(data[0].email)
+	$('#phone').val(data[0].phone)
+	$('#bio').val(data[0].bio)
 }
 
 function userPutRequest(event) {
-  event.preventDefault()
-  // add photo later
-  let putURL = userURL + userId
-  $.ajax({
-    url: putURL,
-    type: 'PUT',
-    data: {
-      name: $('#name').val(),
-      email: $('#email').val(),
-      phone: $('#phone').val(),
-      bio: $('#bio').val()
-    },
-    success: function(result) {
-        // Do something with the result
-      showUserProfile(result)
-    }
-  });
+	event.preventDefault()
+	// add photo later
+	let putURL = userURL + userId
+	$.ajax({
+		url: putURL,
+		type: 'PUT',
+		data: {
+			name: $('#name').val(),
+			email: $('#email').val(),
+			phone: $('#phone').val(),
+			bio: $('#bio').val()
+		},
+		success: function(result) {
+			// Do something with the result
+			showUserProfile(result)
+		}
+	});
+}
+
+function logOut(event) {
+	event.preventDefault()
+	localStorage.removeItem('token')
+	location.href = '/'
 }
