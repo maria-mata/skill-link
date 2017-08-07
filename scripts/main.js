5bde67e6146489b8670f156b6739023ad1297022
Maria Mata <mariajo.mata@gmail.com>
2017-08-06 20:18:06 -0600

    progress on suggested matches and edit skills

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-const userId = 1
+var userId = 1 // this will change based on authentication
 
 $(document).ready(function() {
   $('.button-collapse').sideNav()
@@ -10,8 +10,8 @@ $(document).ready(function() {
   $.get(`https://young-peak-51032.herokuapp.com/users/matches/${userId}`)
     .then(appendSkillMatches)
 
-  $('#user-put').submit(userPutRequest)
-  // $('#skills-put').submit(skillsPutRequest)
+  $('#user-put').submit(updateProfile)
+  // $('#skills-put').submit(updateSkills)
 
 });
 
@@ -22,9 +22,10 @@ function showUserProfile(data) {
   $('#email').val(data[0].email)
   $('#phone').val(data[0].phone)
   $('#bio').val(data[0].bio)
+  Materialize.updateTextFields() // ensure labels don't screw up content
 };
 
-function userPutRequest(event) {
+function updateProfile(event) {
   event.preventDefault()
   $.ajax({
     url: `https://young-peak-51032.herokuapp.com/users/${userId}`,
@@ -42,31 +43,61 @@ function userPutRequest(event) {
   })
 };
 
-// function skillsPutRequest(event) {
-//
-// }
+// returns an array of skills that a user has
+function skillsHave(userId) {
+  let output = []
+  $.get(`https://young-peak-51032.herokuapp.com/users/skills/${userId}`)
+    .then(function(data) {
+      for (let i = 0; i < data.length; i++) {
+        output.push(data[i].name)
+      }
+    })
+}
+
+// returns the name of the skill when passed the skill id
+function skillWant(skillId) {
+  $.get('https://young-peak-51032.herokuapp.com/skills')
+    .then(function(data) {
+      return data.filter((el) => {
+        return el.id == skillId
+      }, []).filter((el) => {
+        return el.name
+      }, [])
+    })
+}
+
+function updateSkills(event) {
+  // get skills they have => users/skills AND /skills - many to many
+  // get skills they want => users/:id AND /skills - one to many
+}
+
+
 
 function appendSkillMatches(data) {
   for (let i = 0; i < data.length; i++) {
-    let skillMatch = `<li class="collection-item avatar">
-                  <img src="http://www.cdn.innesvienna.net//Content/user-default.png" alt="" class="circle">
-                  <span class="title">${data[i].name}</span>
+    let match = `<li class="collection-item avatar">
+                  <img src="http://www.cdn.innesvienna.net//Content/user-default.png"
+                  alt="${data[i].name}" class="circle">
+                  <span class="title"><b>${data[i].name}</b></span>
                   <p>Can Teach You: <br>
-                     Wants to Learn: ${data[i].skill_learn}
+                     Wants to Learn:
+                     <a id="${data[i].id}" href="#match-modal"
+                     class="secondary-content modal-trigger">
+                     <i class="material-icons">remove_red_eye</i></a>
                   </p>
-                  <a href="#!" class="secondary-content"><i class="material-icons">info_outline</i></a>
                 </li>`
-    $('#matches').append(skillMatch)
+    $('#matches').append(match)
+    $(`#${data[i].id}`).click(() => {
+      showMatchProfile(data[i])
+    })
   }
-}
+};
 
-/* SUGGESTED MATCHES APPEND ELEMENT
-`            <li class="collection-item avatar">
-              <img src="http://www.cdn.innesvienna.net//Content/user-default.png" alt="" class="circle">
-              <span class="title">Title</span>
-              <p>First Line <br>
-                 Second Line
-              </p>
-              <a href="#!" class="secondary-content"><i class="material-icons">add</i></a>
-            </li>`
-*/
+function showMatchProfile(match) {
+  $('#match-modal > div.modal-content').empty()
+  let content = `<h4>${match.name}</h4>
+    <ul>
+      <li></li>
+    </ul>`
+  $('#match-modal > div.modal-content').append(content)
+}
